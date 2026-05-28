83d42231a13287db58529d32a3d7d85f16cf72a9
EkaterinaOdints <157913603+EkaterinaOdints@users.noreply.github.com>
2026-05-28 17:44:14 +0300

    fixes for github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
+const repoName = "CoolCard";
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  trailingSlash: true,
+
+  basePath: isProd ? `/${repoName}` : "",
+  assetPrefix: isProd ? `/${repoName}/` : "",
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
